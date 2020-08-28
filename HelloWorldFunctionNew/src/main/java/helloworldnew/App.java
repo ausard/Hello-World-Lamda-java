2001ab5bea9f2a8633feefd753446f0408108640
ausard <ausard@yandex.ru>
2020-08-28 15:21:59 +0300

    add new function

---
+++ HelloWorldFunctionNew/src/main/java/helloworldnew/App.java
@@ -0,0 +1,42 @@
+package helloworldnew;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+
+import helloworldlib.Library;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Handler for requests to Lambda function.
+ */
+public class App implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+
+    public APIGatewayProxyResponseEvent handleRequest(final APIGatewayProxyRequestEvent input, final Context context) {
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Content-Type", "text/html");
+        headers.put("X-Custom-Header", "application/json");
+
+        APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent()
+                .withHeaders(headers);
+        try {
+            String output="";
+            boolean check=new Library().someLibraryMethod();
+            if(!check){
+                output = "<h1>Hello from Aliaksei Sabetski New</h1>";
+            }else {
+                output="<h1>Hello from Aliaksei New</h1>";
+            }
+
+            return response
+                    .withStatusCode(200)
+                    .withBody(output);
+        } catch (Exception e) {
+            return response
+                    .withBody("<b>Error</b>")
+                    .withStatusCode(500);
+        }
+    }
+}
