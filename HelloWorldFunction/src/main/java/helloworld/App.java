7cc16ad4eb62c3255729a6bb72eb61fad6f577e7
ausard <ausard@yandex.ru>
2020-08-28 11:38:55 +0300

    init commit

---
+++ HelloWorldFunction/src/main/java/helloworld/App.java
@@ -0,0 +1,39 @@
+package helloworld;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+
+/**
+ * Handler for requests to Lambda function.
+ */
+public class App implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+
+    public APIGatewayProxyResponseEvent handleRequest(final APIGatewayProxyRequestEvent input, final Context context) {
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Content-Type", "text/html");
+        headers.put("X-Custom-Header", "application/json");
+
+        APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent()
+                .withHeaders(headers);
+        try {      
+            String output = "<h1>Hello from Aliaksei Sabetski</h1>";
+            return response
+                    .withStatusCode(200)
+                    .withBody(output);
+        } catch (Exception e) {
+            return response
+                    .withBody("<b>Error</b>")
+                    .withStatusCode(500);
+        }
+    }    
+}
