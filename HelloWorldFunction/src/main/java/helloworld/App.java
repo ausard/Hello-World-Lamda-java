9cef62d15f9499b063a6d5411666f03fbf10de79
ausard <ausard@yandex.ru>
2020-08-28 15:12:00 +0300

    all done

--- HelloWorldFunction/src/main/java/helloworld/App.java
+++ HelloWorldFunction/src/main/java/helloworld/App.java
@@ -1,17 +1,13 @@
 package helloworld;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.stream.Collectors;
-
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+
+import HelloWorldLib.Library;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Handler for requests to Lambda function.
@@ -25,8 +21,15 @@ public class App implements RequestHandler<APIGatewayProxyRequestEvent, APIGatew
 
         APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent()
                 .withHeaders(headers);
-        try {      
-            String output = "<h1>Hello from Aliaksei Sabetski</h1>";
+        try {
+            String output="";
+            boolean check=new Library().someLibraryMethod();
+            if(!check){
+                output = "<h1>Hello from Aliaksei Sabetski</h1>";
+            }else {
+                output="<h1>Hello from Aliaksei</h1>";
+            }
+
             return response
                     .withStatusCode(200)
                     .withBody(output);
