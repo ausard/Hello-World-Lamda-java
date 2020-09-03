4cd363d602aec6446f73c85d0d9d82bbc2c23aef
ausard <ausard@yandex.ru>
2020-09-03 17:03:00 +0300

    correct

--- HelloWorldFunction/src/main/java/helloworld/App.java
+++ HelloWorldFunction/src/main/java/helloworld/App.java
@@ -29,52 +29,10 @@ public class App implements RequestHandler<APIGatewayProxyRequestEvent, APIGatew
                 output = "<h1>Hello from Aliaksei Sabetski</h1>";
             } else {
                 output = "<h1>Hello from AWS Lambda Function and aws-sam-cli</h1><br>";
-                output = output.concat("<h2>HI String Concatenation by concat() method</h2>");
-                output = output.concat('<div class="boxed"><!-- 
-
-
-                If you want to use Lorem Ipsum within another program please contact us for details
-                on our API rather than parse the HTML below, we have XML and JSON available.
+                output = output.concat("<h2>HI String Concatenation by concat() method1</h2><br>");
+                output = output.concat("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum lacus libero, iaculis viverra tincidunt eget, interdum in sapien. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla scelerisque ligula nec varius hendrerit. Mauris augue dui, egestas eget sem ac, luctus consectetur ligula.<p>" .
+                 "Sed congue metus nec turpis posuere consequat. In mollis sapien et accumsan auctor. Praesent elit massa, imperdiet eget massa quis, bibendum tincidunt nibh. Cras pharetra nibh ante, nec rutrum urna mattis sit amet. Duis eu ipsum metus. Integer porta lacinia risus. Maecenas ullamcorper ante eu faucibus pretium. Donec lacinia lectus nulla. Integer sagittis efficitur ligula, non luctus nisl condimentum id.                Phasellus eget tempor quam, id euismod felis. Morbi non pellentesque diam. Nulla non ultrices erat, nec ultrices libero. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Curabitur elementum luctus feugiat. In tempus tortor id felis lacinia vulputate. Vivamus laoreet tempus tempus. Praesent tincidunt scelerisque porta. Proin ultricies sit amet leo vel faucibus. Mauris nec tincidunt dolor. Quisque vehicula arcu ac iaculis consectetur. Donec malesuada nec velit convallis molestie.");
                 
-                
-                 --><div id="lipsum">
-                <p>
-                <ul>
-                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
-                <li>Vivamus porttitor nisi id velit facilisis, at tempus purus congue.</li>
-                <li>Cras id augue imperdiet, sollicitudin dui sagittis, auctor est.</li>
-                <li>Maecenas ut neque a risus fringilla pellentesque eu eget nunc.</li>
-                <li>Phasellus at nulla iaculis ex semper ornare.</li>
-                </ul>
-                </p>
-                <p>
-                <ul>
-                <li>Aliquam pretium ante sit amet est dignissim, fermentum varius magna pulvinar.</li>
-                <li>Duis eleifend felis non viverra sodales.</li>
-                </ul>
-                </p>
-                <p>
-                <ul>
-                <li>Donec lobortis ipsum a dictum bibendum.</li>
-                <li>Maecenas vestibulum massa nec dolor interdum, in porttitor mauris dignissim.</li>
-                </ul>
-                </p>
-                <p>
-                <ul>
-                <li>Quisque ut orci elementum, ullamcorper augue vitae, semper sapien.</li>
-                <li>Mauris ullamcorper lectus nec augue viverra, sit amet pellentesque velit congue.</li>
-                <li>Maecenas et lectus ac diam sollicitudin scelerisque.</li>
-                </ul>
-                </p>
-                <p>
-                <ul>
-                <li>Maecenas sed felis nec tellus interdum consequat.</li>
-                <li>Vivamus pellentesque dolor sed purus pulvinar commodo.</li>
-                <li>Mauris varius mi nec ex dignissim auctor.</li>
-                <li>Fusce pulvinar sem nec nunc convallis, in lacinia mauris ornare.</li>
-                <li>Quisque tristique quam eu augue rhoncus, at cursus massa pellentesque.</li>
-                </ul>
-                </p></div>');
             }
 
             return response
